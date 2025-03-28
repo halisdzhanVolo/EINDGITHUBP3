8ed4e59784009e5c4c3da2dc6ff359ee042d52da
halisdzhan <6045912@mborijnland.nl>
2025-03-28 10:14:29 +0100

    footer toegevoegd

---
+++ footer.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Responsive Website</title>
+    <link rel="stylesheet" href="css/stylesheet.css">
+    <script src="js/script.js" defer></script>
+    <meta author="Dogukan Arslan">
+    <meta description="Dit is het footer van website">
+    <meta keywords="footer, website, footer">
+</head>
+
+<body>
+
+    <footer>
+        <section class="footer-content">
+            <section class="logo">
+                <img src="images/flogo.png" alt="Umami Sushi Logo">
+            </section>
+            <section class="right-content">
+                <section class="slogan">
+                    <h4>HERE YOU FIND</h4>
+                </section>
+
+                <section class="social-media">
+                    <a href="https://www.instagram.com/" target="_blank" aria-label="Instagram">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                    <a href="https://www.facebook.com/" target="_blank" aria-label="Facebook">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                    <a href="https://x.com/?lang=en" target="_blank" aria-label="Twitter">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                </section>
+            </section>
+        </section>
+        <section class="copyright">
+            <p>&copy; 2024 Your Website. All rights reserved. Privacy Policy Terms of Service</p>
+        </section>
+    </footer>
+
+</body>
+
+</html>
